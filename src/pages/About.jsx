1f0133d7f1f286d4d41f28d0e782792e09fc3c87
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-03-29 17:27:01 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/AboutPage";
+
+
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias perferendis consequuntur magni similique blanditiis eveniet numquam sapiente quo temporibus dolorum enim ratione, dicta itaque, illum, nesciunt at aut incidunt maxime?
+      </p>
+    </Wrapper>
+  );
+};
+
+export default About;
